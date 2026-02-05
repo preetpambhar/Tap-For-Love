6973fc3844a282f6a2fc6b2ffe51745c955d4949
Preet Pambhar <preetpambhar11@gmail.com>
2026-02-04 22:35:45 -0500

    Initial Commit

---
+++ Tap For Love/Tap_For_LoveApp.swift
@@ -0,0 +1,17 @@
+//
+//  Tap_For_LoveApp.swift
+//  Tap For Love
+//
+//  Created by Preet Pambhar on 2026-02-04.
+//
+
+import SwiftUI
+
+@main
+struct Tap_For_LoveApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
